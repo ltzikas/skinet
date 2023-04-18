36bf21950337fd04e05a3f4a418691097e8a308f
Varhöll <lucio.tzikas@gmail.com>
2023-04-18 00:27:56 -0300

    Section 14 - Lesson 148

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ const routes: Routes = [
   {path: 'not-found', component: NotFoundComponent},
   {path: 'server-error', component: ServerErrorComponent},
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule)},
+  {path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule)},
   {path: '**',redirectTo: '', pathMatch: 'full'}
 ];
 
