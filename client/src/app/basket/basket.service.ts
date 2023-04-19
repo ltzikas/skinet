486504f5be619831deb5d1a9878dea4810a2f96d
ltzikas <lucio.tzikas@gmail.com>
2023-04-19 18:12:56 -0300

    Section 14 - Lesson 152

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Basket } from '../shared/models/basket';
+import { IProduct } from "../shared/models/product";
 import { HttpClient } from '@angular/common/http';
+import { BasketItem } from '../shared/models/basket';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +17,7 @@ export class BasketService {
   constructor(private http: HttpClient) { }
 
   getBasket(id: string){
-    return this.http.get<Basket>(this.baseUrl + 'basket?id= ' + id).subscribe({
+    return this.http.get<Basket>(this.baseUrl + 'basket?id=' + id).subscribe({
       next: basket => this.basketSource.next(basket) 
     })
   }
@@ -26,8 +28,45 @@ export class BasketService {
     })
   }
 
-  getCurrent(){
+  getCurrentBasketValue(){
+    console.log(this.basketSource.value);
     return this.basketSource.value;
+  }
+
+  addItemToBasket(item: IProduct, quantity = 1){
+    const itemToAdd = this.mapProductItemToBasketItem(item);
+    const basket = this.getCurrentBasketValue() ?? this.createBasket();
+    basket.items = this.addOrUpdateItem(basket.items, itemToAdd, quantity);
+    this.setBasket(basket);
+  }
+  
+  private addOrUpdateItem(items: BasketItem[], itemToAdd: BasketItem, quantity: number): BasketItem[] {
+    const item = items.find(x => x.id === itemToAdd.id);
+    if (item) item.quantity += quantity;
+    else{
+      itemToAdd.quantity = quantity;
+      items.push(itemToAdd);
+    }
+    return items;
+  }
+
+  createBasket(): Basket {
+    const basket = new Basket();
+    localStorage.setItem('basket_id',basket.id);
+    console.log(basket.id);
+    return basket;
+  }
+
+  private mapProductItemToBasketItem(item: IProduct): BasketItem{
+    return {
+      id: item.id,
+      productName: item.name,
+      price: item.price,
+      quantity: 0,
+      pictureUrl: item.pictureUrl,
+      brand: item.productBrand,
+      type: item.productType
+    }
   }
 
 }
--- client/src/app/shop/product-item/product-item.component.ts
+++ client/src/app/shop/product-item/product-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { BasketService } from 'src/app/basket/basket.service';
 import { IProduct } from 'src/app/shared/models/product';
 
 @Component({
@@ -9,7 +10,12 @@ import { IProduct } from 'src/app/shared/models/product';
 export class ProductItemComponent implements OnInit {
   @Input() product: IProduct;
 
-  constructor() { }
+  constructor(private basketService: BasketService) { }
+
+  addItemToBasket(){
+    this.product && this.basketService.addItemToBasket(this.product);
+  }
+
 
   ngOnInit(): void {
   }
